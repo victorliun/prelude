b3bf53887cbff8e1b05b1966430be68f1cf99748
Victor Liu <victor.ns.liu@outlook.com>
2016-02-19 09:06:28 +0000

    config for fiplr

--- personal/personal.el
+++ personal/personal.el
@@ -33,3 +33,9 @@
 
 ;; auto-completion
 (require 'auto-complete)
+
+;; fiplr config
+(setq fiplr-root-markers '(".git" ".svn"))
+(setq fiplr-ignored-globs '((directories (".git" ".svn"))
+                            (files ("*.jpg" "*.png" "*.zip" "*~"))))
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
